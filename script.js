8cde9baa1ef2eb263c1848eb9181e339a8c67be3
Bahbah89 <jesseadjomocoding@gmail.com>
2023-10-15 14:17:09 +0200

    added validation email checker

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const sub = document.getElementById("subscribe");
 const startForm = document.querySelector(".start-form");
 const thanksForm = document.querySelector(".thanks-message");
 const disMiss = document.getElementById("dismiss");
+let userEmail = document.getElementById("user-email");
+let emailField = document.getElementById("email-field");
+const valid = document.querySelector(".valid");
 
 sub.addEventListener("click", function () {
-  thanksForm.classList.remove("hidden");
-  startForm.style.display = "none";
+  if (!document.getElementById("email-field").checkValidity()) {
+    valid.classList.remove("hidden");
+    emailField.style.backgroundColor = "hsl(4, 60%, 67%)";
+  } else {
+    thanksForm.classList.remove("hidden");
+    startForm.style.display = "none";
+    console.log(userEmail, emailField);
+    userEmail.textContent = emailField.value;
+  }
 });
 
 disMiss.addEventListener("click", function () {
